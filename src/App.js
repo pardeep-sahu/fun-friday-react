321d72fe09c373e867ccadeeda1fa341381f5fa0
naveen.negi <naveen.negi@ldh.01s.in>
2022-05-11 18:35:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// import Home from './components/Home';
+// import Welcome from './components/Welcome';
+import './app.css'
+import React from 'react';
+import Topbar from './components/topbar/Topbar';
+import Sidebar from './components/sidebar/Sidebar';
+import Home from './pages/home/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import UserList from './pages/userList/UserList';
+import User from './pages/user/User';
+import NewUser from './pages/newUser/NewUser';
+import Posts from './pages/posts/posts';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* <h1>This is root componant</h1>
+      // <Home name="Pardeep Sahu" role="Software Developer">
+      // <p>I am working in 01Synergy since last two year</p>
+      // </Home>
+      // <Welcome /> */}
+      <Topbar />
+      <div className='container' style={{ 'maxWidth': '1350px' }}>
+        <Sidebar />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path="/users" element={<UserList />} />
+          <Route path="/posts" element={<Posts />} />
+          <Route path="/user/:userId" element={<User />} />
+          <Route path="/newUser" element={<NewUser />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
